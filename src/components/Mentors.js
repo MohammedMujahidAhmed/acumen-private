a74a541993d346f9ed01633c67ed0969c43f34c6
Sainith123 <yamsanisainith@gmail.com>
2024-04-08 12:06:01 +0530

    edit

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -3,7 +3,6 @@ import hod from '../images/hod.jpeg'
 import principal from '../images/principal.jpeg'
 import jalaja from '../images/jalaja.jpg'
 import Frames from './Frames'
-import circles from '../gif/circles.gif'
 import bhargavi from '../images/bhargavi.jpg'
 
 const data = [
@@ -43,8 +42,6 @@ const Mentors = () => {
   return (
     <section className='flex flex-col relative gap-5 mt-4 transition ease-in-out justify-center items-center text-white' >
         <h3 className='text-3xl'>Mentors</h3>
-        <img src={circles} alt='dot' className='absolute top-0 md:left-[20%] left-[5%]' loading='lazy'/>
-        <img src={circles} alt='dot' className='absolute bottom-0 md:right-[20%] right-[5%]' loading='lazy'/>
         {
             <Frames {...data[index]}/>
         }
